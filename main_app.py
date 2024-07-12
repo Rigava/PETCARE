194da55ce69f0132ac6dc44f2353cd2af634ac22
Rigava <josharun1@gmail.com>
2024-07-12 19:05:42 +0530

    2f

--- main_app.py
+++ main_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pysqlite3
+__import__('pysqlite3')
 import sys
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # import chromadb
 from crewai import Crew, Process, Agent, Task
 from langchain_core.callbacks import BaseCallbackHandler

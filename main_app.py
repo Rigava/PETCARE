720824790ea3a057df8d250833f53198954c9d9e
Rigava <josharun1@gmail.com>
2024-07-12 18:58:33 +0530

    2f

--- main_app.py
+++ main_app.py
@@ -7,10 +7,10 @@ import google.generativeai as genai
 
 
 key =st.secrets.API_KEY
-# from langchain_google_genai import GoogleGenerativeAI
-# llm = GoogleGenerativeAI(model='gemini-pro',google_api_key=key)
-from langchain_community.llms import GooglePalm
-llm = GooglePalm(model ='models/gemini-1.0-pro',google_api_key =key)
+from langchain_google_genai import GoogleGenerativeAI
+llm = GoogleGenerativeAI(model='models/text-bison-001',google_api_key=key)
+# from langchain_community.llms import GooglePalm
+# llm = GooglePalm(model ='models/gemini-1.0-pro',google_api_key =key)
 
 genai.configure(api_key=key)
 

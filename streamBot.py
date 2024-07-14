dcabcd980d90898f6a6e052ad7507b689a922e82
Rigava <josharun1@gmail.com>
2024-07-14 22:44:22 +0530

    5f

--- streamBot.py
+++ streamBot.py
@@ -54,6 +54,7 @@ if user_query is not None and user_query !="":
         st.markdown(user_query)    
     with st.chat_message("ai"):
         # ai_response = get_response(user_query,st.session_state.chat_history)
-        ai_response= st.write_stream(get_response(user_query,st.session_state.chat_history))
+        # st.markdown(ai_response)
+        ai_response = st.write_stream(get_response(user_query,st.session_state.chat_history))
     st.session_state.chat_history.append(AIMessage(ai_response))
 
